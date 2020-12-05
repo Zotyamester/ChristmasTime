1fa25b614d08e044745eccd7524cf18e7d12af65
Zotyamester <szatmary.zoltan1222@gmail.com>
2020-12-05 20:06:08 +0100

    Add function declarations to imgeffect.h

--- imgeffect.h
+++ imgeffect.h
@@ -5,4 +5,8 @@
 
 typedef unsigned char Matrix[HEIGHT][WIDTH];
 
+void screenToMatrix(SDL_Window *window, Matrix *mat);
+void matrixToScreen(SDL_Renderer *renderer, Matrix *mat);
+void blurScreen(SDL_Window *window, SDL_Renderer *renderer);
+
 #endif // IMGEFFECT_H
